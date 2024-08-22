f1d61e5858765cbe31e3769b5d23172f277d52b5
mateo9722 <matemkamelot@hotmail.com>
2024-08-22 11:21:49 -0500

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
 import { Platform } from "../hooks/useGames";
+import { Publisher } from "./publisher";
+import { Genre } from "./useGenres";
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
