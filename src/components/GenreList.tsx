f90a17a2b3337f743068b05c51817fbdb0cf26b6
mateo9722 <matemkamelot@hotmail.com>
2024-08-22 10:22:07 -0500

    Refactor move entity interfaces

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -7,7 +7,7 @@ import {
   ListItem,
   Spinner,
 } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import useGenres from "../entities/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import useGameQueryStore from "../store";
 
